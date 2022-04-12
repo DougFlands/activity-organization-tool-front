ec0ec5a32bb404ac0e6f9e246930fb066eccfa04
DougFlands <czx.flands@gmail.com>
2022-04-13 00:30:51 +0800

    Revert "增加已删除数据的判断"
    This reverts commit 8ba2134777b4986f16a5283d38bc635e87a9759f.

--- src/pages/activity/game-content.tsx
+++ src/pages/activity/game-content.tsx
@@ -110,11 +110,6 @@ const GameContent = props => {
     return n > d
   }
 
-  // 检查是否已删除
-  const checkIsDel = () => {
-    return !!props.data.dateTime
-  }
-
   return (
     <AtCard
       title={`${props.data.busGame.type === 1 ? '剧本' : '桌游'}: ${
@@ -126,10 +121,6 @@ const GameContent = props => {
         {!props.data.edit && props.data.isInvolved ? (
           <View className={style.isInvolved}>已参加</View>
         ) : null}
-        {checkIsDel() ? (
-          <View className={style.isDel}>活动已被删除</View>
-        ) : null}
-
         <View>发起人: {props.data.user.nickName}</View>
         <View>地点: {props.data.location}</View>
         <View>费用: ￥{props.data.price}</View>
@@ -162,7 +153,7 @@ const GameContent = props => {
             参加
           </AtButton>
         ) : null
-      ) : checkDataTime() || props.data.dateTime ? null : (
+      ) : checkDataTime() ? null : (
         <AtButton
           type="primary"
           size="small"
