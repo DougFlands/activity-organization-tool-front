408c95683fb40416ceb4f6a4a858df0cac8a2e94
DougFlands <czx.flands@gmail.com>
2022-04-30 19:42:17 +0800

    增加强制更新

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { AtTabBar } from 'taro-ui'
+import { useModal } from 'taro-hooks'
 import { useStore } from '@/store'
 import Activity from '../activity/index'
 import User from '../user/index'
@@ -9,6 +11,13 @@ import style from './index.scss'
 const Index = () => {
   const { GlobalStore } = useStore()
   const [current, setCurrent] = useState(0)
+  const updateManager = Taro.getUpdateManager()
+  const [showModal] = useModal({
+    title: '更新提示',
+    content: '新版本已经准备好，请重启应用!',
+    showCancel: false,
+  })
+
   const tabList = [
     { title: '活动', iconType: 'home' },
     { title: '我的', iconType: 'user' },
@@ -17,6 +26,23 @@ const Index = () => {
   const handleClick = (page: number) => {
     setCurrent(page)
   }
+
+  const init = async () => {
+    // 检查更新
+    updateManager.onCheckForUpdate(res => {
+      // 请求完新版本信息的回调
+      console.log('检查更新: ', res.hasUpdate)
+    })
+    updateManager.onUpdateReady(async () => {
+      await showModal()
+      // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+      updateManager.applyUpdate()
+    })
+  }
+
+  useEffect(() => {
+    init()
+  })
 
   return (
     <View className={style.appWrapper}>
